2c542eca8491bbc907579a35598182f5b7f49973
abutaher2020 <62004336+abutaher2020@users.noreply.github.com>
2023-02-25 00:42:58 +0600

    Add files via upload

---
+++ php/respons.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="/css/respons.css">
+<link rel="stylesheet" href="/css/navbarmenu1.css">
+
+</head>
+<body style="font-family:Verdana;color:#aaaaaa;">
+  <!-- Nav Bar START-->
+<div class="navbar01 navbar01b">
+  <a href="../index.php" class="navbar01a01"><img id="imgicon" src="/img/icon.png"></a>
+  <a href="#" class="navbar01a02">NBFirst</a>
+  <a href="#" class="navbar01a03">NBSecond</a>
+  <a href="#" class="navbar01a04">NBThird</a>
+</div>
+<!-- Nav Bar END-->
+
+<div style="background-color:#e5e5e5;padding:15px;text-align:center;">
+  <h1>Hello World</h1>
+</div>
+
+<div style="overflow:auto">
+  <div class="menu">
+    <a href="#">HOME</a>
+    <a href="#">TANZIL PHP</a>
+    <a href="#">Link 3</a>
+    <a href="#">Link 4</a>
+  </div>
+
+  <div class="main">
+    <h2>Another Option</h2>
+    <p>There are more option to earn on online. you have to choose one of them.</p>
+  </div>
+
+  <div class="right">
+    <h2>About</h2>
+    <p>You have to choose one of them.</p>
+  </div>
+</div>
+
+<div style="background-color:#e5e5e5;text-align:center;padding:10px;margin-top:7px;">© copyright matsoft.com</div>
+
+</body>
+</html>
---
+++ tanzil.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html >
+<html >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="/css/csstanzil.css">
+<link rel="stylesheet" href="/css/navbarmenu1.css">
+<link rel="stylesheet" href="/css/divbotmenu1.css">
+
+
+<link rel="icon" type="image/x-icon" href="/img/icon.png">
+<title>Page#02</title>
+<style type="text/css">
+
+</style>
+</head>
+<body>
+
+
+<!-- Nav Bar START-->
+<div class="navbar01 navbar01b">
+	<a href="index.php" class="navbar01a01"><img id="imgicon" src="/img/icon.png"></a>
+	<a href="#" class="navbar01a02">NBFirst</a>
+	<a href="#" class="navbar01a03">NBSecond</a>
+	<a href="#" class="navbar01a04">NBThird</a>
+</div>
+<!-- Nav Bar END-->
+<div id="container">
+<p>Second Page</p>
+</div>
+<hr>
+<?php
+$number="This a sample for Variable with echo";
+echo  $number ;
+?>
+<p>
+<hr>
+How to Host an HTML website using IIS on windows machine<hr><br><br>
+https://www.youtube.com/watch?v=Pw79cNSAivE<br>
+https://www.youtube.com/watch?v=abqTcXeyst0
+</p>
+<br>
+<hr>
+<div class="cai">
+<p id="convai">
+Conversational AI is technology that enables humans and computers to communicate clearly and effectively through speech or text. Conversational AI works by recognizing a user’s speech or text patterns, predicting their intent and responding with an adaptive, automated script. The best conversational AI platforms today can create truly natural, human-like interactions.
+
+
+<hr><br>How is conversational AI being used today?<br>
+There is a wide variety of conversational AI applications currently being used across all industries. From customer service to marketing to security, these highly intelligent programs are helping businesses connect with customers and employees in never-before-seen ways. In fact, conversational AI has become a centerpiece of many organizations’ digital transformation in the wake of the global pandemic.
+
+However, not all conversational AI initiatives are created equal. Results have shown that those with the highest success rate are more strategic in their development and deployment than their rush-to-market peers. By first asking thoughtful questions—like, “What are your goals?” “Are you looking to give more information to your web visitors?” “Are you looking to generate leads?”—organizations can tailor the technology to their needs rather than rely on “plug-and-play” chatbots, voice assistants and other solutions.
+
+As the technology continues to advance, new opportunities to leverage conversational AI continue to emerge. Future applications already in development include home automation, automobiles, marketing, advertising and e-learning. As our exposure to and appetite for interactive technology increases, conversational AI will play a bigger and bigger role in our everyday lives.
+
+<hr><br>The technology behind conversational AI<br>
+Conversational AI as we know it is driven by three technological developments. First, natural language processing (NLP)  software analyzes natural human language and speech, interpreting contextual nuances and extracting relevant information. Artificial intelligence (AI) then uses this data to predict patterns of communication. Lastly, machine learning (ML) enables AI-based systems to “learn” and improve from experience without being explicitly programmed. Today, NLP, AI and ML power a range of applications, including search engines, email spam filters, language translation software, voice assistant, grammar analysis and even social media management applications.
+
+<hr><br>Agent greeting Customer<br>
+Why are businesses investing in conversational AI?<br>
+Businesses today are investing heavily in conversational AI for a number of reasons. With new applications emerging in nearly every industry, conversational AI represents a major potential revenue source. Among the biggest beneficiaries of the conversational AI boom is customer service. This innovative technology allows customer service departments—many of which operate with limited resources—to easily:
+
+<hr><br>Hold a personalized conversation at scale<br>
+According to a famous adage, the internet has not changed customer expectations, but it has empowered them. Customers today expect a world-class experience from even the smallest organizations. Conversational AI helps deliver a personalized customer conversation at scale across multiple channels. So, whether a customer moves from a messenger app to a live chat or social channels, they’ll experience a seamless, highly personalized customer journey.
+
+<hr><br>Manage spikes in customer call volumes<br>
+For customer service departments, higher call volumes are part of the new, post-pandemic reality. Using conversational AI, chatbots and voice assistants can help resolve low-value calls and relieve beleaguered customer service teams during sudden call spikes.
+
+With conversational AI, it is possible to categorize calls based on what the customer is looking for, their past interactions with the brand, and their emotions, sentiments and intents. Routine, transactional interactions can be routed to an intelligent virtual assistant (IVR), thus lowering the costs of high-touch interactions while also empowering live agents to focus on higher-value conversations.
+
+<hr><br>Deliver on the customer service promise<br>
+Today, customer experience matters more than ever, outpacing product and pricing as the single biggest brand differentiator. According to Forbes, 82 percent of customers will stop doing business with a company after a bad customer experience. And roughly 80 percent will tell others about their experience.
+
+Conversational AI can help organizations optimize their customer service promise by creating a more customer-centric experience. By augmenting self-service—and live agent assistance—with intent, emotion and sentiment analysis, conversational AI can increase first contact resolution (FCR) and customer satisfaction scores.
+
+<hr><br>How does conversational AI benefit businesses?<br>
+Conversational AI benefits businesses in multiple, diverse ways—from lead and demand generation to customer service and beyond. Many enterprises deploy these AI-based tools to help sales teams upsell and cross-sell more efficiently. And as the technology continues to advance and mature, new applications will aid and/or automate additional areas of operation.
+
+Here are some examples of how businesses are currently using conversational AI to drive value.
+
+<hr><br>Optimize customer service<br>
+Whether customers realize it or not, conversational AI has become an integral part of today’s customer service fabric. Most major brands offer (or plan to offer) some form of chatbot and voice bot assistance; those that don’t are increasingly in the minority. By applying conversational AI, NLP and ML to understand the intricacies of human speech, language and emotions, businesses can offer a more positive, personalized experience than their competitors. Furthermore, these solutions scale seamlessly, allowing for easy, enterprise-wide implementations.
+
+<hr><br>Drive Marketing and sales efforts<br>
+Customers today have higher expectations—and more options—than ever before. Conversational AI can give businesses a competitive advantage by driving better targeting and conversion for marketing and sales teams. AI-based solutions can better understand the buyer profile, their social media preferences, role, etc. and offer relevant content that’s tailored to their personality and properly timed to drive conversion. They can also create more meaningful interactions by accommodating customers’ emotion, sentiment and intent.
+
+<hr><br>Driving marketing and sales efforts<br>
+Conversational AI is the next evolutionary step in CX<br>
+ai assisted conversational ai automation<br>
+As customer expectations evolve, so do the tools customer service providers use to deliver engaging CX. Conversational AI, ML and NLP—together with smarter machines and advanced modelling—are enabling businesses to interact with their customers with ever greater efficiency, effectiveness and empathy.
+
+What’s more, these technologies allow for a seamless, highly personalized customer journey across multiple channels. So even if a customer first engages a company via text or chatbot, their relevant information—including intent, sentiment and emotional analysis—can be shared with a live agent if needed. This flexibility is particularly valuable as customers increasingly prefer multichannel communication.
+
+While conversational AI is driving the next generation in customer service, the journey has just begun. As consumers become increasingly confident and comfortable with AI-based interactions, the trend toward greater conversational automation will become the norm. In the coming years, conversational AI will play a larger role in our everyday interactions, particularly as businesses look to:
+
+<hr><br>Automate multiple processes<br>
+Not all tasks require human attention. Conversational AI can quickly resolve low-effort emails and calls, freeing customer service representatives to focus on more complex interactions. By automating the bulk of these tasks, conversational AI can drastically reduce contact center operational costs and even errors associated with manual data entry. Additionally, the technology can uncover insights that human representatives would otherwise be unable to recognize.
+
+
+<hr><br>Augment agent capabilities<br>
+Automating multiple processes​<br>
+Conversational AI isn’t rendering contact agents redundant (despite earlier fears); it’s empowering them to be more efficient and effective. Today’s agents are facing record call volumes, including a 50 percent increase in complex/difficult calls, according to the Harvard Business Review. Conversational AI can augment agent capabilities with actionable insights, automated knowledge bases, alerts and notifications—all during live customer interactions. For example, if an agent forgets to disclose important information necessary for compliance, an AI-enabled program can send him or her a reminder. Furthermore, by recording relevant data in real-time, conversational AI can dramatically reduce time-consuming post-call work.
+
+<hr><br>Improve decision making<br>
+Unlike humans, machines can perform the same task again and again with the same level of efficiency and accuracy. Conversational AI is no different. As a result, business leaders are using this technology to help customer service employees make better decisions. Conversational AI offers employees a deeper understanding of customer concerns, including context and background. Guided by this information, employees can then take the next-best actions best suited for each situation.
+
+<hr><br>Augmenting agent capabilities​<br>
+Reduce response times<br>
+Reducing query response time<br>
+Today’s customers are impatient. They demand immediate resolution for their issues. Conversational AI can radically reduce response times across all channels—from social media, email, web chat and phone traffic to outside queries from review sites and third-party aggregators. That’s because conversational AI can digest thousands of emails, chats, social media threads and even phone calls in a fraction of the time it would take humans. By letting machines do the fact-finding “grunt work”, live representatives (or self-service bots) can speed up the time to resolution and satisfy need-it-now customers.
+
+<hr><br>Personalize customer experience.<br>
+Conversational AI can give each customer experience a level of personalization not possible until now. While it might seem counterintuitive that machines can understand humans better than other humans, the reality is just the opposite. Unlike humans, machines purely on data—without passions, distractions or lapses in memory. Conversational AI can understand every customer, their proclivities and unique set of problems and create a highly customized resolution journey.
+
+Conversational AI tools can collect and analyze historical customer data and extract valuable insights from it. They can build detailed customer personas, including likes, dislikes and other personal preferences. These machines can also detect patterns, trends and behavioral cues from customers’ speech that live representatives might easily miss.
+
+By personalizing experiences, businesses get a more complete picture of their customers—particularly what drives them to act and what causes them to churn. Conversational AI can help anticipate problems before they arise and offer ready solutions to prevent customer churn. In the same way that it can identify individual sales and lead opportunities, conversational AI can also target areas that impact retention.
+
+<hr><br>Boost revenue and bottom-line growth<br>
+At the end of the day, businesses need solutions that drive revenue and bottom-line growth. Conversational AI helps reduce customer service costs by taking over the tasks that would normally require human effort. This translates to real savings—in HR and personnel costs, overhead costs and more. At the same time, by improving customer experience, conversational AI can accelerate revenue generation and growth, enticing customers to spend more and promote your brand to others.
+
+<hr><br>Boosting revenue and bottom-line<br>
+Final thoughts<br>
+Businesses today are realizing that conversational AI is a critical component of a truly competitive customer service strategy. As customer expectations continue to rise, AI implementations will be a lifeline for enterprises of every industry, delivering consistently high-level customer service at scale. From automated self-service to live agent assistance, conversational AI connects people and machines—often across vast networks and multiple channels—allowing them to communicate more clearly and effectively than ever thought possible. And this is just the beginning.
+
+As we enter a new era of customer service, our expectations and capabilities will continue to define one another. Today’s innovations, including conversational AI and automation, are fueling tomorrow’s digital transformations. Those that leverage this technology effectively will outperform their peers—now and for years to come.
+
+Get in touch with Uniphore to learn more about how you can benefit from Conversational AI now!
+</p>
+</div>
+<!-- Menu Table  Bar start-->
+<div style="overflow:auto">
+  <div class="divbotmenu1">
+  	<table id="tbmenu1">
+  		<tr id="tbmenu1tr1">
+  			<td id="td1" class="tbmenu1td"><a href="index.php">HOME</a></td>
+  			<td class="tbmenu1td"><a href="http://localhost:8081/php/respons.php">Hellow World</a></td>
+  			<td class="tbmenu1td"><a href="#">Link 3</a></td>
+  			<td class="tbmenu1td"><a href="#">Link 4</a></td>
+  		</tr>
+  	</table>    
+  </div>
+</div>
+<!-- Menu Table  Bar END-->
+</body>
+</html>
+
+
+
